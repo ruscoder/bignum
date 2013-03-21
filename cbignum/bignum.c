12d3b861ba39b0124cc44a2b8776faec3234a43b
ruscoder <rusmiligamer@mail.ru>
2013-03-21 19:33:23 +0800

    Added bigPlus and bigMinus

--- cbignum/bignum.c
+++ cbignum/bignum.c
@@ -4,12 +4,23 @@
 #include <math.h>
 #include <ctype.h>
 
-#define BASE 1000 * 1000 * 1000
-#define DIGITS 9
-#define DIGITSD 9.
+#define BASE 1000 * 1000 * 100
+#define DIGITS 8
+#define DIGITSD 8.
+#define OUT_FORMAT "%.8d"
 
-struct BigInt bigFromFile(const char *name) {
-	struct BigInt res;
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+struct BigNum newBigNum(int len) {
+	struct BigNum res;
+	res.len = len;
+	res.digits = calloc(len, sizeof(int));
+	return res;
+}
+
+
+struct BigNum bigFromFile(const char *name) {
+	struct BigNum res;
 	
 	FILE *fp = fopen(name, "r+");
 	if (fp == NULL) {
@@ -20,28 +31,27 @@ struct BigInt bigFromFile(const char *name) {
 	// Get file size
 	fseek(fp, 0L, SEEK_END);
 	int size = ftell(fp);
-	res.len = ceil(size / DIGITSD);
-	res.digits = calloc(res.len, sizeof(int));
-	
+	res = newBigNum(ceil(size / DIGITSD));
 	// Seek to start
 	fseek(fp, 0l, SEEK_SET);
-	
 	// Read file
-	int i;
+	char buf[DIGITS + 1];
+	int i, 
+		offset = size % DIGITS,
+		count = 0;
+	
 	for (i = res.len - 1; i >= 0; --i) {
-		char buf[DIGITS + 1];
-		int readed = fread(buf, 1, DIGITS,  fp);
-		buf[readed] = 0;
-
-		// Parse str digit to int	
-		int j, temp = 0;
-		for (j = 0; j < readed; ++j) {
-			if (!isdigit(buf[j]))
-				break;
-			temp *= 10;
-			temp += buf[j] - '0';
+		if (offset == 0) {
+			// Read full num
+			count = DIGITS;
+		} else {
+			// Read first part
+			count = offset;
+			offset = 0;
 		}
-		res.digits[i] = temp;
+		int readed = fread(buf, 1, count,  fp);
+		buf[readed] = 0;
+		res.digits[i] = atoi(buf);
 	}
 
 	fclose(fp);
@@ -49,15 +59,60 @@ struct BigInt bigFromFile(const char *name) {
 }
 
 
-void bigToFile(const char *name, struct BigInt first) {
+void bigToFile(const char *name, struct BigNum first) {
 	FILE *fp = fopen(name, "w+");
 	if (fp == NULL) {
 		printf("Cannot open file %s", name);
 		return;
 	}
 	int i;
-	for (i = first.len - 1; i >= 0; --i) {
-		fprintf(fp, "%d", first.digits[i]);
+	fprintf(fp, "%d", first.digits[first.len - 1]);
+	for (i = first.len - 2; i >= 0; --i) {
+		fprintf(fp, OUT_FORMAT, first.digits[i]);
 	}
 	fclose(fp);
 }
+
+struct BigNum bigPlus(struct BigNum first, struct BigNum second) {
+	struct BigNum res = newBigNum(MAX(first.len, second.len) + 1);
+	int i;
+	unsigned int cur = 0;
+	for (i = 0; i < res.len; ++i) {
+		if (i < first.len)
+			cur += first.digits[i];
+		if (i < second.len)
+			cur += second.digits[i];
+		ldiv_t div_res = ldiv(cur, BASE);
+		res.digits[i] = div_res.rem; // %
+		cur = div_res.quot; // div
+	}
+	if (res.digits[res.len - 1] == 0)
+		res.len--;
+	return res;
+}
+
+struct BigNum bigMinus(struct BigNum first, struct BigNum second) {
+	// A >= B always
+	struct BigNum res = newBigNum(MAX(first.len, second.len));
+	int i;
+	int cur = 0;
+	for (i = 0; i < res.len; ++i) {
+		cur += first.digits[i];
+		if (i < second.len) {
+			if (first.digits[i] < second.digits[i]) {
+				cur += BASE;
+				res.digits[i + 1] = -1;
+			}
+			cur -= second.digits[i];
+		}
+		ldiv_t div_res = ldiv(cur, BASE);
+		res.digits[i] = div_res.rem; // %
+		cur = div_res.quot; // div
+	}
+	//res = removeLeadNulls(res);	
+	return res;
+}
+
+
+
+
--- cbignum/bignum.h
+++ cbignum/bignum.h
@@ -1,7 +1,10 @@
-struct BigInt {
+struct BigNum {
 	int * digits;
 	int len;
 };
 
-struct BigInt bigFromFile(const char *);
-void bigToFile(const char *, struct BigInt);
+struct BigNum bigFromFile(const char *);
+void bigToFile(const char *, struct BigNum);
+struct BigNum bigPlus(struct BigNum, struct BigNum);
+struct BigNum bigMinus(struct BigNum, struct BigNum);
+
